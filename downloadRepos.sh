bee2502c5106e6fcbc0eb43dd03222ba4e4b6ec3
StrayRaider <41emrekaya919@gmail.com>
2023-08-16 23:01:48 +0300

    repo downloader code added for me

---
+++ downloadRepos.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+echo "Downloading all repos here ! "
+
+
+git clone git@github.com:StrayRaider/SolarSdl.git
+git clone git@github.com:StrayRaider/sdl_android_build.git
+git clone git@github.com:StrayRaider/ProLab.git 
+git clone git@github.com:Meta-Land/MetaLand.git 
+git clone git@github.com:StrayRaider/maze_solver_robot.git 
+git clone git@github.com:StrayRaider/ytdl_gui.git
+git clone git@github.com:StrayRaider/website.git
+git clone git@github.com:StrayRaider/StrayRaider.git 
+git clone git@github.com:StrayRaider/snakeGD.git
+git clone git@github.com:StrayRaider/Game-Machine.git
+git clone git@github.com:StrayRaider/Cv_works.git
+git clone git@github.com:StrayRaider/SolarSkate.git
+git clone git@github.com:StrayRaider/java_works.git
+git clone git@github.com:StrayRaider/wallpapers.git 
+git clone git@github.com:StrayRaider/pytui.git
+git clone git@github.com:StrayRaider/School_pdfs.git
+git clone git@github.com:StrayRaider/titrek.git
+git clone git@github.com:StrayRaider/gtk-notes.git
+git clone git@github.com:StrayRaider/my_kras.git
+git clone git@github.com:StrayRaider/solargolf.git
